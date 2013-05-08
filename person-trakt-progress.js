6f3590656b42f518f4708455fa0529c48779653c
mmounirou <mmounirou@mmounirou.com>
2013-05-08 10:18:34 +0200

    refactor js

--- person-trakt-progress.js
+++ person-trakt-progress.js
@@ -1,40 +1,41 @@
- $.get(chrome.extension.getURL("person-trakt-progress.ms.html"),function(data){
-    var template = Handlebars.compile(data);
-
-     function parseKind(href){
-        var elmt = $(href).attr("href").split("/").reverse();
-        return elmt[1]+"-"+elmt[0];
-     }
-     
-    function insertProgression(kindId){
-        var kindDiv = $("#"+kindId);
-        var allMovies = kindDiv.find(".library-show");
-        
-        var movieCount = allMovies.length;
-        
-        var collectedCount = allMovies.find(".episode-overlay-collection").length;
-        var seenCount = allMovies.find(".episode-overlay-seen").length;
-        var scrobbledCount = allMovies.find(".episode-overlay-watched").length;
-        var watchedCount = seenCount + scrobbledCount;
-        
-        var context = {all:movieCount,
-                       watched:watchedCount,
-                       collected:collectedCount,
-                       remainWatched:movieCount - watchedCount,
-                       remainCollected:movieCount - collectedCount,
-                       progressPercent:Math.round(watchedCount*100/movieCount),
-                       collectPercent:Math.round(collectedCount*100/movieCount)
-                      }
-
-        $("#progress-toggler").remove();
-        $(".main-wrapper").before(template(context));        
-    }
-
+function updateProgression(template){
+    var elmt = $("#role-links>a.current").attr("href").split("/").reverse();
+    var kindId =  elmt[1]+"-"+elmt[0];
+    var kindDiv = $("#"+kindId);
+    var allMovies = kindDiv.find(".library-show");
     
-    insertProgression(parseKind($("#role-links>a.current")));
+    var movieCount = allMovies.length;
+    
+    var collectedCount = allMovies.find(".episode-overlay-collection").length;
+    var seenCount = allMovies.find(".episode-overlay-seen").length;
+    var scrobbledCount = allMovies.find(".episode-overlay-watched").length;
+    var watchedCount = seenCount + scrobbledCount;
+    
+    var context = {all:movieCount,
+                   watched:watchedCount,
+                   collected:collectedCount,
+                   remainWatched:movieCount - watchedCount,
+                   remainCollected:movieCount - collectedCount,
+                   progressPercent:Math.round(watchedCount*100/movieCount),
+                   collectPercent:Math.round(collectedCount*100/movieCount)
+                  }
+
+    $("#progress-toggler").remove();
+    $(".main-wrapper").before(template(context));        
+}
+
+$.get(chrome.extension.getURL("person-trakt-progress.ms.html"),function(data){
+    var template = Handlebars.compile(data);
+    
+    updateProgression(template);
     
     $("#role-links>a").click(function(){
-        insertProgression(parseKind($(this)));
+        updateProgression(template);
     });
 
+    $(".seen, .collection").hover(function(){
+        //updateProgression(template);
+        console.log("hover");
+    });
+    
 }); 
