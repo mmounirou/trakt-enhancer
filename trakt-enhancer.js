6b9d7855da3e1466184cbda4d68992307cd1d217
mmounirou <mmounirou@mmounirou.com>
2013-05-24 20:20:06 +0200

    add yify support on movie page

--- trakt-enhancer.js
+++ trakt-enhancer.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-	if(window.location.href.indexOf("http://trakt.tv/person") == 0){
+	if (window.location.href.indexOf("http://trakt.tv/person") == 0) {
 		initTraktProgress();
 	}
 
 	initYifyEnhancement();
 
 
-    //Fix an issue in trakt which allow the user to mark an item as seen or collected indefinitly
-    $(".seen, .collection").click(function(){
-        $(this).remove();
-    });
+	//Fix an issue in trakt which allow the user to mark an item as seen or collected indefinitly
+	$(".seen, .collection").click(function() {
+		$(this).remove();
+	});
 })
