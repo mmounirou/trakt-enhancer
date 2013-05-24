ff3d9ae40ee8e8386faeef63f2f35f290edfd233
mmounirou <mmounirou@mmounirou.com>
2013-05-24 20:05:47 +0200

    refactor

--- yify-enhancer.js
+++ yify-enhancer.js
@@ -1,10 +1,5 @@
-function initYifyEnhancement(){
-	console.log("");
-
-	$(".library-show , .poster").has('a[href^="/movie"]').each(function(index,data){
-		var title = $(data).find(".title").text();
-		
-		var request = $.ajax({
+function queryYify(title,success){
+   		var request = $.ajax({
 			url: "http://yify-torrents.com/api/list.json",
 			type: "GET",
 			data: "limit=" + "1" + "&quality=" + "720p" +"&keywords="+title,
@@ -13,16 +8,26 @@ function initYifyEnhancement(){
 
 		request.done(function(result) {
             if(!(result["error"]==="No movies found")){
-                $(data).find(".general").append('<span class="download show-tip" title="Download" />');
                 var movieTorrentUrl = result["MovieList"][0]["TorrentUrl"];
-                $(data).find(".download").click(function(){
+                success(movieTorrentUrl);
+            }
+		}); 
+}
+
+function initYifyEnhancement(){
+	console.log("");
+
+    if(window.location.href.indexOf("http://trakt.tv/movie") == 0){
+		initTraktProgress();
+	}
+    
+	$(".library-show , .poster").has('a[href^="/movie"]').each(function(index,data){
+		var title = $(data).find(".title").text();
+		queryYify(title,function(movieTorrentUrl){
+             $(data).find(".general").append('<span class="download show-tip" title="Download" />');
+            $(data).find(".download").click(function(){
                     location.href = movieTorrentUrl;
                 });
-            }
-		});
-
-		request.fail(function(jqXHR, textStatus) {
-			alert( "Request failed: " + textStatus );
-		});
+        });
 	})
 }
