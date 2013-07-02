090adad80322f3cd0692c915f1d459a4851009ad
mmounirou <mmounirou@cantor.fr>
2013-07-02 17:25:29 +0200

    update the yify query to use the new search by ImdbId params

--- yify-enhancer.js
+++ yify-enhancer.js
@@ -1,8 +1,8 @@
-function queryYify(title, success) {
+function queryYify(imdbId, success) {
     var request = $.ajax({
         url: "http://yify-torrents.com/api/list.json",
         type: "GET",
-        data: "limit=" + "1" + "&quality=" + "720p" + "&keywords=" + title,
+        data: "limit=" + "1" + "&quality=" + "720p" + "&keywords=" + imdbId,
         dataType: "json"
     });
 
@@ -18,8 +18,11 @@ function initYifyEnhancement() {
     console.log("");
 
     if (window.location.href.indexOf("http://trakt.tv/movie") == 0) {
-        var title = $("#below-header h2").text();
-        queryYify(title, function(movieTorrentUrl) {
+        console.log();
+        var imdbId = $('#meta-imdb-id').attr("value");
+        //var title = $("#below-header h2").text();
+
+        queryYify(imdbId, function(movieTorrentUrl) {
             $("#vip-refresh-wrapper").append('<img id="download-torrents" class="show-tip" title="" src="http://b.dryicons.com/images/icon_sets/coquette_part_5_icons_set/png/128x128/magnet.png">');
             $("#download-torrents").click(function() {
                 location.href = movieTorrentUrl;
@@ -27,9 +30,12 @@ function initYifyEnhancement() {
         });
     }
 
-    $(".library-show , .poster").has('a[href^="/movie"]').each(function(index, data) {
+    $(".library-show , .poster").has('a[href^="/movie"]').has('.add-to-list').each(function(index, data) {
         var title = $(data).find(".title").text();
-        queryYify(title, function(movieTorrentUrl) {
+        var parsedJSON = $.parseJSON($(data).find(".add-to-list").attr('rel'));
+        var imdbId = parsedJSON.imdb_id;
+        console.log(title + " - " + imdbId);
+        queryYify(imdbId, function(movieTorrentUrl) {
             $(data).find(".general").append('<span class="download show-tip" title="Download" />');
             $(data).find(".download").click(function() {
                 location.href = movieTorrentUrl;
