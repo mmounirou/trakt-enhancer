5c984f97581268164e17ebc5dad17c230e83cf27
mmounirou <mmounirou@cantor.fr>
2014-01-17 17:57:51 +0100

    restore yify support

--- traktenhancer-main.js
+++ traktenhancer-main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	console.log("");
 	traktweb.fixIssues();
+	traktweb.addYifyDownload();
 	traktweb.addPeopleProgressionChart();
 	traktweb.addMovieReleaseLocalDate();
 	traktweb.addCharts();
