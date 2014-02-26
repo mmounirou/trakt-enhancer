da2b24b9fb7a119855cd35762ca05f7339aa5041
mmounirou <mmounirou@cantor.fr>
2014-02-26 17:45:23 +0100

    cache yts torrent link

--- libs/yify-lib.js
+++ libs/yify-lib.js
@@ -2,20 +2,28 @@ window.yifyapi = (function() {
 	function YifyApi() {}
 
 	YifyApi.prototype.getTorrentUrl = function(imdbId, quality, callback) {
-		var request = $.ajax({
-			url: "http://yify-torrents.com/api/list.json",
-			type: "GET",
-			data: "limit=" + "1" + "&quality=" + quality + "&keywords=" + imdbId,
-			dataType: "json"
-		});
 
-		request.done(function(result) {
-			var movieTorrentUrl;
-			if (!(result["error"] === "No movies found")) {
-				movieTorrentUrl = result["MovieList"][0]["TorrentUrl"];
-			}
-			callback(movieTorrentUrl);
-		});
+		var cachedLink = localStorage.getItem("yts."+quality+"."+imdbId);
+		if(cachedLink != null){
+			callback(cachedLink);
+		}
+		else{
+			var request = $.ajax({
+				url: "http://yify-torrents.com/api/list.json",
+				type: "GET",
+				data: "limit=" + "1" + "&quality=" + quality + "&keywords=" + imdbId,
+				dataType: "json"
+			});
+
+			request.done(function(result) {
+				var movieTorrentUrl;
+				if (!(result["error"] === "No movies found")) {
+					movieTorrentUrl = result["MovieList"][0]["TorrentUrl"];
+					localStorage.setItem("yts."+quality+"."+imdbId,movieTorrentUrl);		
+				}
+				callback(movieTorrentUrl);
+			});	
+		}
 	};
 
 	return new YifyApi();
